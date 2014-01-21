7753057b5444662d01d36d612d8e5dc2ceaebe02
hieutran106 <kcq_hieutt16@Wifi.local>
2014-01-21 10:14:14 +0700

    init project

---
+++ OpenSnow/OpenSnow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenSnow
+//
+//  Created by KCQ_HIEUTT16 on 1/21/14.
+//  Copyright (c) 2014 KCQ_HIEUTT16. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenSnow/OpenSnow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OpenSnow
+//
+//  Created by KCQ_HIEUTT16 on 1/21/14.
+//  Copyright (c) 2014 KCQ_HIEUTT16. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
